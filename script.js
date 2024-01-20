ea0c951f69aee78cf854d9828a9cb774631c475b
devandaniel9 <85812999+devandaniel9@users.noreply.github.com>
2024-01-20 18:44:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function w3_open() {
 function w3_close() {
     document.getElementById("mySidebar").style.display = "none";
 }
+
+document.getElementById("boxlink").innerHTML = `
+<a href="index.html">Home</a>
+<a href="blog.html">Blog</a>
+<a href="fakta.html">Fakta</a>
+<a href="profil.html">Profil</a>
+<a href="portofolio.html">Portofolio</a>
+<a href="hasil-karya.html">Hasil Karya</a>
+<a href="timeline.html">Timeline</a>
+<a href="updates.html">Updates</a>
+<a href="projects.html">Projects</a>
+`;
