2c52f0b3d56002f65da3ca3544933f42eb46b755
devandaniel9 <85812999+devandaniel9@users.noreply.github.com>
2024-03-04 13:49:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-}
+// Script
 
-function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
+// function w3_open() {
+//     document.getElementById("mySidebar").style.display = "block";
+// }
+
+// function w3_close() {
+//     document.getElementById("mySidebar").style.display = "none";
+// }
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("mybacksidebar").style.display = "block";
+    document.getElementById("mybacksidebar").style.width = "100%";
+    document.getElementById("mybacksidebar").style.height = "100%";
+    // document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
+}
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("mybacksidebar").style.display = "none";
+    document.getElementById("mybacksidebar").style.width = "0";
+    document.getElementById("mybacksidebar").style.height = "0";
+    // document.body.style.backgroundColor = "rgba(0,0,0,0)";
 }
 
 document.getElementById("boxlink").innerHTML = `
