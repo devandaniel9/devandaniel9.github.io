4778268cc311b3575cecac5c7241a731253ff08d
devandaniel9 <85812999+devandaniel9@users.noreply.github.com>
2024-03-07 22:37:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,28 +9,53 @@
 // }
 
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("mybacksidebar").style.display = "block";
-    document.getElementById("mybacksidebar").style.width = "100%";
-    document.getElementById("mybacksidebar").style.height = "100%";
+    document.getElementById("sidebar").style.width = "300px";
+    document.getElementById("backsidebar").style.display = "block";
+    document.getElementById("backsidebar").style.width = "100%";
+    document.getElementById("backsidebar").style.height = "100%";
     // document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
 }
 function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("mybacksidebar").style.display = "none";
-    document.getElementById("mybacksidebar").style.width = "0";
-    document.getElementById("mybacksidebar").style.height = "0";
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("backsidebar").style.display = "none";
+    document.getElementById("backsidebar").style.width = "0";
+    document.getElementById("backsidebar").style.height = "0";
     // document.body.style.backgroundColor = "rgba(0,0,0,0)";
 }
 
-document.getElementById("boxlink").innerHTML = `
-<a href="index.html">Home</a>
-<a href="blog.html">Blog</a>
-<a href="fakta.html">Fakta</a>
-<a href="profil.html">Profil</a>
-<a href="portofolio.html">Portofolio</a>
-<a href="hasil-karya.html">Hasil Karya</a>
-<a href="timeline.html">Timeline</a>
-<a href="updates.html">Updates</a>
-<a href="projects.html">Projects</a>
+// document.getElementById("boxlink").innerHTML = `
+document.getElementById("sidebar").innerHTML = `
+<div class="closebtn" onclick="closeNav()"><img src="images/close-icon.png" width="20"></div>
+<span style="font-size:26px;"><b>Main Menu</b></span><br>
+<span style="font-size:20px;"><b>Halaman</b></span>
+<input type="text" id="mySearch" onkeyup="myFunction()" placeholder="Search..."><br>
+<hr style="height:2px; border-width:0; background-color:gray">
+<ul class="heading" id="myMenu">
+	<li><a href="index.html">Home</a></li>
+	<li><a href="blog.html">Blog</a></li>
+	<li><a href="fakta.html">Fakta</a></li>
+	<li><a href="profil.html">Profil</a></li>
+	<li><a href="portofolio.html">Portofolio</a></li>
+	<li><a href="hasil-karya.html">Hasil Karya</a></li>
+	<li><a href="timeline.html">Timeline</a></li>
+	<li><a href="updates.html">Updates</a></li>
+	<li><a href="projects.html">Projects</a></li>
+</ul>
+<span style="font-size:20px; color:gray; position:absolute; bottom:16px">About Website</span>
 `;
+
+function myFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("mySearch");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myMenu");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
