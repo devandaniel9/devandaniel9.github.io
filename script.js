7745d24d798d06741db2ff750bd2912b59fd275b
devandaniel9 <85812999+devandaniel9@users.noreply.github.com>
2024-04-25 23:57:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -31,31 +31,35 @@ document.getElementById("sidebar").innerHTML = `
 <input type="text" id="mySearch" onkeyup="myFunction()" placeholder="Search..."><br>
 <hr style="height:2px; border-width:0; background-color:gray">
 <ul class="heading" id="myMenu">
-	<li><a href="index.html">Home</a></li>
-	<li><a href="blog.html">Blog</a></li>
-	<li><a href="fakta.html">Fakta</a></li>
-	<li><a href="profil.html">Profil</a></li>
-	<li><a href="portofolio.html">Portofolio</a></li>
-	<li><a href="hasil-karya.html">Hasil Karya</a></li>
-	<li><a href="timeline.html">Timeline</a></li>
-	<li><a href="updates.html">Updates</a></li>
-	<li><a href="projects.html">Projects</a></li>
+    <li><a href="index.html">Home</a></li>
+    <li><a href="blog.html">Blog</a></li>
+    <li><a href="fakta.html">Fakta</a></li>
+    <li><a href="profil.html">Profil</a></li>
+    <li><a href="portofolio.html">Portofolio</a></li>
+    <li><a href="hasil-karya.html">Hasil Karya</a></li>
+    <li><a href="timeline.html">Timeline</a></li>
+    <li><a href="updates.html">Updates</a></li>
+    <li><a href="projects.html">Projects</a></li>
+    <li><a style="color:red">Pertanyaan</a></li>
+    <li><a style="color:red">Guide</a></li>
+    <li><a style="color:red">Explore</a></li>
 </ul>
-<span style="font-size:20px; color:gray; position:absolute; bottom:16px">About Website</span>
+<a style="font-size:20px; color:red; width:100%; position:absolute; bottom:25px;">About Website</a>
 `;
+// <span style="font-size:20px; color:gray; position:absolute; bottom:16px">About Website</span>
 
 function myFunction() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("mySearch");
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myMenu");
-  li = ul.getElementsByTagName("li");
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("mySearch");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myMenu");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
     }
-  }
 }
