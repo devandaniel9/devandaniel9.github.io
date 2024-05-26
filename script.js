d3279455e97b6d6cbcecbb807549190f828949be
devandaniel9 <85812999+devandaniel9@users.noreply.github.com>
2024-05-26 20:28:55 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -24,61 +24,22 @@ function closeNav() {
 }
 
 // document.getElementById("boxlink").innerHTML = `
+document.getElementById("top-container2").innerHTML = `
+<div class="sidebar" id="sidebar"></div>
+<div class="backsidebar" id="backsidebar" onclick="closeNav()"></div>
+<div class="column" style="padding-right:30px;">
+    <div class="openbtn" onclick="openNav()"><img src="images/menu-icon.jpg" width="30"></div>
+</div>
+<div class="column" style="padding-right:20px;">
+    <!-- <img src="images/blank-profile.jpg" width="40" style="border-radius:50%; padding-top:2px"> -->
+    <img src="images/profile.jpg" style="height:46px; border-radius:50%;">
+</div>
+<div style="font-size:30px; color:white; padding-top:4px;"><b>MY WEBSITE</b></div>
+<div class="clearfix"></div>
+`
+
+// document.getElementById("boxlink").innerHTML = `
 document.getElementById("sidebar").innerHTML = `
-<style>
-/* The Modal (background) */
-.modal {
-    display: none; /* Hidden by default */
-    position: fixed; /* Stay in place */
-    z-index: 1; /* Sit on top */
-    padding-top: 100px; /* Location of the box */
-    left: 0;
-    top: 0;
-    width: 100%; /* Full width */
-    height: 100%; /* Full height */
-    overflow: auto; /* Enable scroll if needed */
-    background-color: rgb(0,0,0); /* Fallback color */
-    background-color: rgba(0,0,0,0.3); /* Black w/ opacity */
-}
-
-/* Modal Content */
-.modal-content {
-    background-color: #fefefe;
-    border-radius: 10px;
-    margin: auto;
-    padding-top: 15px;
-    border: 1px solid #888;
-    width: 80%;
-}
-
-/* The Close Button */
-.close {
-    color: #aaaaaa;
-    float: right;
-    font-size: 28px;
-    font-weight: bold;
-}
-
-.close:hover, .close:focus {
-    color: #000;
-    text-decoration: none;
-    cursor: pointer;
-}
-
-.closebtn2 {
-    background-color: #d0d0d0;
-    border-radius: 6px;
-    cursor: pointer;
-    float: right;
-    margin-right: 15px;
-    font-size: 0px;
-    transition: 0.3s;
-}
-.closebtn2 img {
-    padding: 8px;
-}
-</style>
-
 <div class="closebtn" onclick="closeNav()"><img src="images/close-icon.png" width="20"></div>
 <span style="font-size:26px;"><b>Main Menu</b></span><br>
 <span style="font-size:20px;"><b>Halaman</b></span>
@@ -86,21 +47,21 @@ document.getElementById("sidebar").innerHTML = `
 <hr style="height:2px; border-width:0; background-color:gray">
 
 <ul class="heading" id="myMenu">
-    <li><a href="index.html">Home</a></li>
+    <li><a href="index.html"><img style="vertical-align:middle;" src="images/home-icon.png" width="20">&emsp;Home</a></li>
     <li><a href="blog.html">Blog</a></li>
     <li><a href="fakta.html">Fakta</a></li>
-    <li><a href="profil.html">Profil</a></li>
+    <li><a href="profil.html"><img style="vertical-align:middle;" src="images/profile-icon.png" width="20">&emsp;Profil</a></li>
     <li><a href="portofolio.html">Portofolio</a></li>
     <li><a href="hasil-karya.html">Hasil Karya</a></li>
-    <li><a href="timeline.html">Timeline</a></li>
-    <li><a href="updates.html">Updates</a></li>
+    <li><a href="timeline.html"><img style="vertical-align:middle;" src="images/history-icon.png" width="20">&emsp;Timeline</a></li>
+    <li><a href="updates.html"><img style="vertical-align:middle;" src="images/update-icon.png" width="20">&emsp;Updates</a></li>
     <li><a href="projects.html">Projects</a></li>
     <li><a style="color:red">Pertanyaan</a></li>
     <li><a style="color:red">Guide</a></li>
     <li><a style="color:red">Explore</a></li>
 </ul>
-<!-- <a style="font-size:20px; color:red; width:100%; position:absolute; bottom:25px;">About Website</a> -->
-<button class="heading" id="myBtn" style="font-size:20px; color:red; width:100%; position:absolute; text-align:left; padding-left:20px; bottom:25px;">About Website</button>
+<a style="font-size:20px; color:red; width:100%; position:absolute; bottom:25px;" onclick="openModal()">About Website</a>
+<!-- <button class="heading" id="myBtn" style="font-size:20px; color:red; width:100%; position:absolute; text-align:left; padding-left:20px; bottom:25px;">About Website</button> -->
 
 <!-- The Modal -->
 <div id="myModal" class="modal">
@@ -111,7 +72,7 @@ document.getElementById("sidebar").innerHTML = `
         <span style="font-size:26px;"><b>About Website</b></span><br>
         <p style="padding-left:20px">
             ..........<br>..........<br>..........<br>..........<br>..........<br><br><br>
-            Terakhir diupdate: 8 Mei 2024
+            Terakhir diupdate: 11 Mei 2024
         </p>
     </div>
 </div>
@@ -137,29 +98,13 @@ function myFunction() {
 // Get the modal
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+function openModal() {
     modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
 // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
-
 function closeModal() {
     modal.style.display = "none";
 }
