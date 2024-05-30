1e2dbe9b6e4f003f9fe781dbb67bf1928260826b
devandaniel9 <85812999+devandaniel9@users.noreply.github.com>
2024-05-30 23:46:28 +0700

    Add files via upload

---
+++ blog-description.js
@@ -0,0 +1,5 @@
+// Blog for description
+
+document.getElementById("desc01").innerHTML = "Semarang adalah blablabla";
+document.getElementById("desc01").style.width = "300px";
+document.getElementById("desc02").innerHTML = "Blablabla adalah blablabla";
--- script.js
+++ script.js
@@ -108,3 +108,26 @@ function openModal() {
 function closeModal() {
     modal.style.display = "none";
 }
+
+
+// Get the modal image
+var modal_image = document.getElementById("myModalImage");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal_image.style.display = "block";
+    modalImg.src = this.src;
+    // captionText.innerHTML = this.alt;
+    captionText.innerHTML = document.getElementById("image-desc").innerHTML;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal_image.style.display = "none";
+}
