d90902e9f421572f19b0ea35e03bb537ce1f5ba3
devandaniel9 <85812999+devandaniel9@users.noreply.github.com>
2025-12-14 23:24:50 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ document.getElementById("top-container2").innerHTML = `
     <img src="images/profile.jpg" style="height:46px; border-radius:50%;">
 </div>
 <div class="column" style="padding-right:20px;">
-    <div style="font-size:30px; color:white; padding-top:4px;"><b>MY WEBSITE</b></div>
+    <div class="heading-top" style="font-size:30px; color:white; padding-top:4px;"><b>MY WEBSITE</b></div>
 </div>
 <div class="clearfix"></div>
 `
@@ -72,6 +72,23 @@ theme_check = localStorage.getItem("theme_check");
 if (theme_check == null) theme_check = false;
 console.log(theme_check);
 apply_theme(theme_check);
+
+/*
+<ul class="heading" id="myMenu">
+    <li><a href="index.html"><img style="vertical-align:middle;" src="images/home-icon.png" width="20">&emsp;Home</a></li>
+    <li><a href="blog.html">Blog</a></li>
+    <li><a href="fakta.html">Fakta</a></li>
+    <li><a href="profil.html"><img style="vertical-align:middle;" src="images/profile-icon.png" width="20">&emsp;Profil</a></li>
+    <li><a href="portofolio.html">Portofolio</a></li>
+    <li><a href="hasil-karya.html">Hasil Karya</a></li>
+    <li><a href="timeline.html"><img style="vertical-align:middle;" src="images/history-icon.png" width="20">&emsp;Timeline</a></li>
+    <li><a href="updates.html"><img style="vertical-align:middle;" src="images/update-icon.png" width="20">&emsp;Updates</a></li>
+    <li><a href="projects.html">Projects</a></li>
+    <li><a style="color:red">Pertanyaan</a></li>
+    <li><a style="color:red">Guide</a></li>
+    <li><a style="color:red">Explore</a></li>
+</ul>
+*/
 
 // document.getElementById("boxlink").innerHTML = `
 document.getElementById("sidebar").innerHTML = `
@@ -83,14 +100,12 @@ document.getElementById("sidebar").innerHTML = `
 
 <ul class="heading" id="myMenu">
     <li><a href="index.html"><img style="vertical-align:middle;" src="images/home-icon.png" width="20">&emsp;Home</a></li>
-    <li><a href="blog.html">Blog</a></li>
-    <li><a href="fakta.html">Fakta</a></li>
+    <li><a href="blog.html"><img style="vertical-align:middle;" src="images/blog-icon.png" width="20">&emsp;Blog</a></li>
     <li><a href="profil.html"><img style="vertical-align:middle;" src="images/profile-icon.png" width="20">&emsp;Profil</a></li>
-    <li><a href="portofolio.html">Portofolio</a></li>
-    <li><a href="hasil-karya.html">Hasil Karya</a></li>
+    <li><a href="hasil-karya.html"><img style="vertical-align:middle;" src="images/Achievement-icon-vector-02.svg" width="20">&emsp;Hasil Karya</a></li>
     <li><a href="timeline.html"><img style="vertical-align:middle;" src="images/history-icon.png" width="20">&emsp;Timeline</a></li>
     <li><a href="updates.html"><img style="vertical-align:middle;" src="images/update-icon.png" width="20">&emsp;Updates</a></li>
-    <li><a href="projects.html">Projects</a></li>
+    <li><a href="projects.html"><img style="vertical-align:middle;" src="images/project-icon.png" width="20">&emsp;Projects</a></li>
     <li><a style="color:red">Pertanyaan</a></li>
     <li><a style="color:red">Guide</a></li>
     <li><a style="color:red">Explore</a></li>
@@ -214,53 +229,37 @@ function modal_image_default() {
 	modal_image_desc.innerHTML = document.getElementById("img001d").innerHTML;
 } */
 
-document.getElementById("img001").onclick = function() {
+/* document.getElementById("img001").onclick = function() {
     modal_image_default();
 	modal_image.src = document.getElementById("img001").src;
 	modal_image_desc.innerHTML = document.getElementById("img001d").innerHTML;
-}
+} */
 
-document.getElementById("img002").onclick = function() {
-    modal_image_default();
-	modal_image.src = document.getElementById("img002").src;
-	modal_image_desc.innerHTML = document.getElementById("img002d").innerHTML;
-}
-
-document.getElementById("img003").onclick = function() {
-    modal_image_default();
-	modal_image.src = document.getElementById("img003").src;
-	modal_image_desc.innerHTML = document.getElementById("img003d").innerHTML;
-}
-
-document.getElementById("img004").onclick = function() {
-    modal_image_default();
-	modal_image.src = document.getElementById("img004").src;
-	modal_image_desc.innerHTML = document.getElementById("img004d").innerHTML;
-}
-
-document.getElementById("img005").onclick = function() {
-    modal_image_default();
-	modal_image.src = document.getElementById("img005").src;
-	modal_image_desc.innerHTML = document.getElementById("img005d").innerHTML;
-}
-
-document.getElementById("img006").onclick = function() {
-    modal_image_default();
-	modal_image.src = document.getElementById("img006").src;
-	modal_image_desc.innerHTML = document.getElementById("img006d").innerHTML;
-}
-
-document.getElementById("img007").onclick = function() {
-    modal_image_default();
-	modal_image.src = document.getElementById("img007").src;
-	modal_image_desc.innerHTML = document.getElementById("img007d").innerHTML;
-}
-
-document.getElementById("img008").onclick = function() {
-    modal_image_default();
-	modal_image.src = document.getElementById("img008").src;
-	modal_image_desc.innerHTML = document.getElementById("img008d").innerHTML;
-}
+// try {MathJax.typeset();} catch(err) {}
+try {document.getElementById("img001").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img001").src; modal_image_desc.innerHTML = document.getElementById("img001d").innerHTML;}} catch(err) {}
+try {document.getElementById("img002").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img002").src; modal_image_desc.innerHTML = document.getElementById("img002d").innerHTML;}} catch(err) {}
+try {document.getElementById("img003").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img003").src; modal_image_desc.innerHTML = document.getElementById("img003d").innerHTML;}} catch(err) {}
+try {document.getElementById("img004").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img004").src; modal_image_desc.innerHTML = document.getElementById("img004d").innerHTML;}} catch(err) {}
+try {document.getElementById("img005").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img005").src; modal_image_desc.innerHTML = document.getElementById("img005d").innerHTML;}} catch(err) {}
+try {document.getElementById("img006").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img006").src; modal_image_desc.innerHTML = document.getElementById("img006d").innerHTML;}} catch(err) {}
+try {document.getElementById("img007").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img007").src; modal_image_desc.innerHTML = document.getElementById("img007d").innerHTML;}} catch(err) {}
+try {document.getElementById("img008").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img008").src; modal_image_desc.innerHTML = document.getElementById("img008d").innerHTML;}} catch(err) {}
+try {document.getElementById("img009").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img009").src; modal_image_desc.innerHTML = document.getElementById("img009d").innerHTML;}} catch(err) {}
+try {document.getElementById("img010").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img010").src; modal_image_desc.innerHTML = document.getElementById("img010d").innerHTML;}} catch(err) {}
+try {document.getElementById("img011").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img011").src; modal_image_desc.innerHTML = document.getElementById("img011d").innerHTML;}} catch(err) {}
+try {document.getElementById("img012").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img012").src; modal_image_desc.innerHTML = document.getElementById("img012d").innerHTML;}} catch(err) {}
+try {document.getElementById("img013").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img013").src; modal_image_desc.innerHTML = document.getElementById("img013d").innerHTML;}} catch(err) {}
+try {document.getElementById("img014").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img014").src; modal_image_desc.innerHTML = document.getElementById("img014d").innerHTML;}} catch(err) {}
+try {document.getElementById("img015").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img015").src; modal_image_desc.innerHTML = document.getElementById("img015d").innerHTML;}} catch(err) {}
+try {document.getElementById("img016").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img016").src; modal_image_desc.innerHTML = document.getElementById("img016d").innerHTML;}} catch(err) {}
+try {document.getElementById("img017").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img017").src; modal_image_desc.innerHTML = document.getElementById("img017d").innerHTML;}} catch(err) {}
+try {document.getElementById("img018").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img018").src; modal_image_desc.innerHTML = document.getElementById("img018d").innerHTML;}} catch(err) {}
+try {document.getElementById("img019").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img019").src; modal_image_desc.innerHTML = document.getElementById("img019d").innerHTML;}} catch(err) {}
+try {document.getElementById("img020").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img020").src; modal_image_desc.innerHTML = document.getElementById("img020d").innerHTML;}} catch(err) {}
+try {document.getElementById("img021").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img021").src; modal_image_desc.innerHTML = document.getElementById("img021d").innerHTML;}} catch(err) {}
+try {document.getElementById("img022").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img022").src; modal_image_desc.innerHTML = document.getElementById("img022d").innerHTML;}} catch(err) {}
+try {document.getElementById("img023").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img023").src; modal_image_desc.innerHTML = document.getElementById("img023d").innerHTML;}} catch(err) {}
+try {document.getElementById("img024").onclick = function() {modal_image_default(); modal_image.src = document.getElementById("img024").src; modal_image_desc.innerHTML = document.getElementById("img024d").innerHTML;}} catch(err) {}
 
 for (let i = 5; i <= 4; i++) {
 	img_i = "img" + addzero(i);
